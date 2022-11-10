2d0cf8cb95c898e34d195e180a421616122aac94
Tom <tomadamlee02@gmail.com>
2022-11-10 23:37:45 +0000

    updates post req

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.post('/', (req, res) => {
-  console.log(req.body);
-  res.send(req.query.email);
+  console.log(req.body.email);
+  res.json({ newEmail: 'Success' });
 });
 
 app.get('/', (req, res) => {
