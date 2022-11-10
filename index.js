33e7859397cf8df35111a95ea62ba19f74d775b8
Tom <tomadamlee02@gmail.com>
2022-11-10 20:50:12 +0000

    updated server file for deta

--- index.js
+++ index.js
@@ -10,6 +10,4 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+module.exports = app;
