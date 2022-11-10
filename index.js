676362866a9681280849201e4dd219cfb295542a
Tom <tomadamlee02@gmail.com>
2022-11-10 20:46:26 +0000

    adds deta folder and frontend response

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.post('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
