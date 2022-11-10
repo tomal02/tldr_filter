8a3c1ba78d8c7d4a270c270086e870be2d011755
Tom <tomadamlee02@gmail.com>
2022-11-10 22:47:28 +0000

    readds local listen script

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const app = express();
 const port = 3000;
 
 app.post('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(req.email);
 });
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+if (process.env.NODE_ENV === 'development') {
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
+}
 
 module.exports = app;
